3ad14b6dd87bdb5f3530d58407b6fb1e5fbc7de9
Hamilton <hamilton.gomes8@hotmail.com>
2022-11-22 23:00:29 -0300

    organização de exercícios

--- Exercícios - estágio/senhor.py
+++
@@ -1,9 +0,0 @@
-def init():
-    valorInserido = 0
-    moedasAceitas = [1, 5, 10, 25]
-
-
-def inserirMoeda(moedas):
-    for x in moedas:
-        valorInserido = input("Insira valores de moedas válidos (1, 5, 10 ou 25 centavos).")
-
