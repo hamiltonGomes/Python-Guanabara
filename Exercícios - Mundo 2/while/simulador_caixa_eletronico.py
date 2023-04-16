2ee87d94f36a012851a18935067a7bc8a4c19c28
Hamilton <hamilton.gomes8@hotmail.com>
2023-04-16 20:50:18 -0300

    ✔ Desafio 71 concluído.

---
+++ Exercícios - Mundo 2/while/simulador_caixa_eletronico.py
@@ -0,0 +1,22 @@
+print("=" * 30)
+print("Bem vindo ao Caixa Eletrônico!")
+print("=" * 30)
+
+valor_desejado = int(input("Qual valor deseja sacar? \n"))
+total = valor_desejado
+cedulas_disponiveis = [50, 20, 10, 1]
+total_cedulas = 0
+
+if valor_desejado % min(cedulas_disponiveis) != 0:
+    print("Valor inválido. O valor desejado não pode ser sacado com as cédulas disponíveis.")
+else:
+    for cedula in cedulas_disponiveis:
+        while True:
+            if total >= cedula:
+                total -= cedula
+                total_cedulas += 1
+            else:
+                if total_cedulas > 0:
+                    print(f"Foram utilizadas {total_cedulas} de {cedula}.")
+                total_cedulas = 0
+                break
