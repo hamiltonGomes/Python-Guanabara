f3ab32869a54086c88582cc55647b1c45d6acde4
Hamilton <110085367+hamiltonGomes@users.noreply.github.com>
2023-09-23 01:06:47 -0300

    ✅: challenge done

---
+++ challenges/mario_challenge_harvard.py
@@ -0,0 +1,16 @@
+"""
+The challenge is to create a Mario-style staircase,
+similar to the ones found in the final stages of the Mario Bros. game.
+"""
+
+validate = True
+
+while validate:
+    stair_height = int(input("Digite um número de 1 a 8:"))
+    if stair_height >= 9 or stair_height <= 0:
+        validate = True
+    else:
+        validate = False
+
+for i in range(0, stair_height + 1):
+    print(" " * (stair_height - i) + "#" * i)
